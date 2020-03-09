aeb6d2161848970945548b8906d6165668b68ff0
Dmitry <dimk00z@gmail.com>
2020-03-09 14:41:31 +0300

    Исправление ошибок
    Использован UPPER_CASE для переменных окружения.
    Исправлена загрузка комикса
    Оптимизировано декодирование JSON
    Проверен HTTP статус ответа
    Удаляются временные файлы в случае ошибки

--- env.py
+++ env.py
@@ -1,9 +1,7 @@
 from os import getenv
-from pathlib import Path
 from dotenv import load_dotenv
 
 
 def get_data_from_env(data_name):
-    env_path = Path('.') / '.env'
-    load_dotenv(dotenv_path=env_path)
+    load_dotenv()
     return getenv(data_name)
--- main.py
+++ main.py
@@ -2,16 +2,25 @@ import requests
 from load_image import download_image
 from random import randint
 from env import get_data_from_env
-from os import remove
+from os import remove, path
+
+
+def get_last_xkcd_comic_number():
+    url = 'https://xkcd.com/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()['num']
 
 
 def download_random_comic(image_folder='images'):
-    random_comic_url = f'https://xkcd.com/{randint(1,2277)}/info.0.json'
+    random_comic_number = randint(1, get_last_xkcd_comic_number())
+    random_comic_url = f'https://xkcd.com/{random_comic_number}/info.0.json'
     response = requests.get(random_comic_url)
     response.raise_for_status()
-    image_url = response.json()['img']
-    image_title = response.json()['safe_title']
-    image_comment = response.json()['alt']
+    image_data = response.json()
+    image_url = image_data['img']
+    image_title = image_data['safe_title']
+    image_comment = image_data['alt']
     image_file_name = download_image(image_url, image_folder)
     return (image_file_name, f'{image_title}. {image_comment}')
 
@@ -22,19 +31,23 @@ def get_vk_url_for_upload(vk_access_token, vk_group_id):
               "v": "5.103",
               'group_id': vk_group_id}
     response = requests.get(vk_group_url, params=params)
-    response.raise_for_status()
-    return response.json()['response']['upload_url']
+    json_data = response.json()
+    if 'error' in json_data:
+        raise requests.exceptions.HTTPError(json_data['error'])
+    return json_data['response']['upload_url']
 
 
-def vk_upload_image(url, file_name):
+def upload_image_to_vk(url, file_name):
     with open(file_name, 'rb') as file:
         files = {
             'file1': file
         }
         response = requests.post(url, files=files)
-        response.raise_for_status()
         file.close()
-        return response.json()
+        json_data = response.json()
+        if 'error' in json_data:
+            raise requests.exceptions.HTTPError(json_data['error'])
+        return json_data
 
 
 def vk_save_image(upload_image_vk_response, vk_group_id, vk_access_token):
@@ -47,8 +60,10 @@ def vk_save_image(upload_image_vk_response, vk_group_id, vk_access_token):
               'server': upload_image_vk_response['server']
               }
     response = requests.post(vk_save_url, params=params)
-    response.raise_for_status()
-    return response.json()
+    json_data = response.json()
+    if 'error' in json_data:
+        raise requests.exceptions.HTTPError(json_data['error'])
+    return json_data
 
 
 def vk_wall_post(vk_save_image_response, vk_group_id,
@@ -64,28 +79,35 @@ def vk_wall_post(vk_save_image_response, vk_group_id,
               'attachments': f'photo{photo_owner_id}_{photo_id}'
               }
     response = requests.post(vk_wall_post_url, params=params)
-    response.raise_for_status()
-    return response.json()
+    json_data = response.json()
+    if 'error' in json_data:
+        raise requests.exceptions.HTTPError(json_data['error'])
+    return json_data
 
 
 def vk_post_random_image(vk_user_access_token, vk_group_id):
-    image_file_name, image_comment = download_random_comic()
-    vk_url_for_upload = get_vk_url_for_upload(
-        vk_user_access_token, vk_group_id)
-    upload_image_vk_response = vk_upload_image(
-        vk_url_for_upload, image_file_name)
-    vk_save_image_response = vk_save_image(upload_image_vk_response,
-                                           vk_group_id, vk_user_access_token)
-    vk_wall_post_response = vk_wall_post(
-        vk_save_image_response, vk_group_id,
-        vk_user_access_token, image_comment)
-    remove(image_file_name)
+    try:
+        image_file_name, image_comment = download_random_comic()
+        vk_url_for_upload = get_vk_url_for_upload(
+            vk_user_access_token, vk_group_id)
+        upload_image_vk_response = upload_image_to_vk(
+            vk_url_for_upload, image_file_name)
+        vk_save_image_response = vk_save_image(upload_image_vk_response,
+                                               vk_group_id, vk_user_access_token)
+        vk_wall_post_response = vk_wall_post(
+            vk_save_image_response, vk_group_id,
+            vk_user_access_token, image_comment)
+    except requests.exceptions.HTTPError as ex:
+        print(f'Connection error: {ex}')
+    finally:
+        if path.isfile(image_file_name):
+            remove(image_file_name)
 
 
 def main():
-    vk_group_id = get_data_from_env('group_id')
-    vk_user_access_token = get_data_from_env('user_access_token')
-    vk_post_random_image(vk_user_access_token, vk_group_id)
+    VK_GROUP_ID = get_data_from_env('group_id')
+    VK_USER_ACCESS_TOKEN = get_data_from_env('user_access_token')
+    vk_post_random_image(VK_USER_ACCESS_TOKEN, VK_GROUP_ID)
 
 
 if __name__ == '__main__':
